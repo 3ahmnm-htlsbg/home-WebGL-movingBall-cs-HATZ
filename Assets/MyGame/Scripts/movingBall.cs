2fba306c3ca63702ecaf451954f7bb401eafbe4c
Noah Hatz <Noah@Noah-Hatz-MacBook.local>
2020-01-13 19:08:00 +0100

    Add Script. Begin to write.

---
+++ Assets/MyGame/Scripts/movingBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movingBall : MonoBehaviour
+{
+
+    public Text Title_txt;
+    public Button MoveBall_Btn;
+    public Object Ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TranslateBall()
+    {
+
+    }
+}
