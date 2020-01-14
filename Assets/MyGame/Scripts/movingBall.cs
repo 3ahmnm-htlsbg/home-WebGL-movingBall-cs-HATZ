849a97a216e97d28040032220717b27c8cb9a856
Noah Hatz <Noah@Noah-Hatz-MacBook.local>
2020-01-14 18:16:02 +0100

    Modify README.

--- Assets/MyGame/Scripts/movingBall.cs
+++ Assets/MyGame/Scripts/movingBall.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class movingBall : MonoBehaviour
+public class MovingBall : MonoBehaviour
 {
 
     public GameObject ball;
